6dfab522c8949286666ca6bc7951e438c8234937
Felipe Brandão <felipe.nascimento@duxus.com.br>
2014-08-15 20:06:46 -0300

    Panel Reordering, Menu Selector always visible, Tasks div removed

--- 1.57x/app.js
+++ 1.57x/app.js
@@ -2,10 +2,16 @@
  * Overrides JS for Jenkins
  */ 
 document.observe("dom:loaded", function () {
-    //Changes logo for text:
+    // Changes logo for text:
     jQuery('#jenkins-home-link').html('<img id="jenkins-head-icon" alt="title" src="/static/57065818/images/headshot.png"> Jenkins');
     jQuery('#jenkins-head-icon').show();
-    // Click logo, go home.
-    $$('div.logo')[0].on('click', function(){ location = '/'; });
     
+    // Click logo, go home:
+    $$('div.logo')[0].on('click', function(){ location = '/'; });
+	
+	// Move panels:
+    jQuery('#main-panel').insertBefore('#side-panel');
+    jQuery('#main-panel,#side-panel').removeClass().addClass('col-md-12');
+    jQuery('#side-panel').removeClass().addClass('col-md-12');
+    jQuery('#tasks').remove();
 });
